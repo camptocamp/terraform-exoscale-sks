9b7919a85ed2036ae74dedb7e1f09bdddcdd209c
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-17 15:54:48 +0100

    Defined dependencies

--- versions.tf
+++ versions.tf
@@ -4,5 +4,13 @@ terraform {
       source  = "exoscale/exoscale"
       version = "~> 0.22"
     }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }

0867c656960b2ba8d1c30b3278a1074369112098
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-17 15:23:33 +0100

    Allow to specify cluster name

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "exoscale_sks_cluster" "cluster" {
+resource "exoscale_sks_cluster" "this" {
   zone    = var.zone
-  name    = "prod"
+  name    = var.name
   version = var.kubernetes_version
 }
 
-resource "exoscale_sks_nodepool" "nodepool" {
+resource "exoscale_sks_nodepool" "this" {
   for_each = var.nodepools
 
   zone          = var.zone
-  cluster_id    = exoscale_sks_cluster.cluster.id
+  cluster_id    = exoscale_sks_cluster.this.id
   name          = each.key
   instance_type = each.value.instance_type
   size          = each.value.size
--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
-variable "kubernetes_version" {
-  type    = string
-  default = "1.20.2"
+variable "name" {
+  description = "The name of the SKS cluster."
+  type        = string
 }
 
 variable "zone" {
   description = "The name of the zone to deploy the SKS cluster into."
   type        = string
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.20.2"
 }
 
 variable "nodepools" {

d6c5f203964329bf59f152b03ce4d00b634bd278
Mickaël Canévet <mickael.canevet@gmail.com>
2021-02-18 07:44:51 +0100

    Rename admin user in kubeconfig

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -4,6 +4,6 @@ set -e
 
 eval "$(jq -r '@sh "CLUSTER_ID=\(.cluster_id) ZONE=\(.zone)"')"
 
-kubeconfig=$(exo sks kubeconfig "$CLUSTER_ID" admin --group system:masters --zone "$ZONE")
+kubeconfig=$(exo sks kubeconfig "$CLUSTER_ID" kube-admin --group system:masters --zone "$ZONE")
 
 jq -n --arg kubeconfig "$kubeconfig" '{"kubeconfig":$kubeconfig}'

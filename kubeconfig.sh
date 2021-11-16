2e75ffc6d6d2aa53662c49f512de8b779754fabd
Raphaël Pinson <raphael.pinson@camptocamp.com>
2021-11-16 23:33:36 +0100

    fix(kubeconfig): use monochrome output
    Signed-off-by: Raphaël Pinson <raphael.pinson@camptocamp.com>

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -6,4 +6,4 @@ eval "$(jq -r '@sh "CLUSTER_ID=\(.cluster_id) ZONE=\(.zone)"')"
 
 kubeconfig=$(exo compute sks kubeconfig "$CLUSTER_ID" kube-admin --group system:masters --zone "$ZONE")
 
-jq -n --arg kubeconfig "$kubeconfig" '{"kubeconfig":$kubeconfig}'
+jq -M -n --arg kubeconfig "$kubeconfig" '{"kubeconfig":$kubeconfig}'
